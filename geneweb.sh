c5fb541f6230017c3cbe3be61680ce0a95239c84
Jonny Rimkus <jonny@rimkus.it>
2020-04-13 22:03:01 +0200

    small script improvements

--- geneweb.sh
+++ geneweb.sh
@@ -1,13 +1,49 @@
 #!/bin/sh
 
-docker build -t raver/geneweb .
+start() {
+	docker build -t raver/geneweb .
 
-mkdir -p /var/log/geneweb/
-docker run -d -t \
- -p 2317:2317 \
- -p 2316:2316 \
- -l raver/geneweb \
- -v /var/log/geneweb:/var/log/geneweb \
- --name geneweb \
- raver/geneweb:latest \
- genweb-launch.sh
+	mkdir -p /var/log/geneweb/
+	docker run -d -t \
+	 -p 2317:2317 \
+	 -p 2316:2316 \
+	 -l raver/geneweb \
+	 -v /var/log/geneweb:/var/log/geneweb \
+	 --name geneweb \
+	 raver/geneweb:latest \
+	 genweb-launch.sh
+}
+
+stop() {
+	docker stop geneweb
+	docker rm geneweb
+	status
+}
+
+status(){
+	docker ps
+}
+
+usage(){
+	echo "$(basename $0) start|stop|restart|status"
+}
+
+case $1 in
+
+	start)
+		start
+	;;
+
+	stop)
+		stop
+	;;
+
+	status)
+		status
+	;;
+
+	*)
+		usage
+	;;
+esac
+
